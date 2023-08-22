c0857ad3cccb7fae6836ef5b7c1d2a9db21104b5
Laurensius Yuda Wicaksono Nugroho <yudalaurensius87@gmail.com>
2023-08-22 22:51:23 +0700

    update kondisi sensor gas

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ float vref = 5;
 float totalbit = 32;
 float rangeADC = 4096;
 
+// menentukan ambang PPM untuk mendetksi LPG 
+float ambang_nilai_gas_aman = 200.0;
+float ambang_nilai_gas_normal = 1000.0;
+float ambang_nilai_gas_berbahaya = 5000.0;
+
 
 // void setup
 void setup() {
@@ -40,9 +45,22 @@ void loop() {
   float nilaiADCgas = baca_nilai_adc(readsensorgas);
   float nilaippmgas = baca_nilai_gas(nilaiADCgas);
   float nilaiapi = baca_nilai_api();
-  
+
   // tampilkan pada serial print 
   Serial.println(nilaippmgas);
   Serial.println(nilaiADCgas);
+  Serial.println("Deteksi Api: " + String(nilaiapi == HIGH ? "Ya" : "Tidak"));
+
+
   // membuat kondisi dimana jika membaca adanya gas dan api 
+  // Kondisi untuk mendeteksi konsentrasi gas LPG
+  if (nilaippmgas < ambang_nilai_gas_aman) {
+    Serial.println("Konsentrasi Gas Aman");
+  } else if (nilaippmgas < ambang_nilai_gas_normal) {
+    Serial.println("Konsentrasi Gas Normal");
+  } else if (nilaippmgas < ambang_nilai_gas_berbahaya) {
+    Serial.println("Konsentrasi Gas Berpotensi Bahaya");
+  } else {
+    Serial.println("Konsentrasi Gas Berbahaya");
+  }
 }
