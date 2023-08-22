cfc1b0082b6dfc75f0940deb53bbbb87fac8609b
Laurensius Yuda Wicaksono Nugroho <yudalaurensius87@gmail.com>
2023-08-22 21:23:39 +0700

    update sensor api

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,48 @@
 #include <Arduino.h>
 
-// inisialiasai sensor gas 
+// inisialiasai sensor gas  dan sensor api 
 #define readsensorgas 32
+#define readsensorfire 4
+
+// kalibrasi sensor gas 
+float vin = 3.3;
+float vref = 5;
+float totalbit = 32;
+float rangeADC = 4096;
+
 
 // void setup
 void setup() {
-
+  Serial.begin(9600);
 }
 
-//Read ADC
+//Read ADC sensor gas dan sensor api
 float baca_nilai_adc(int pin){
   int nilaiADC = analogRead(pin);
   return nilaiADC;
 }
 
-//ADC Konversi Menuju PPM ESP 32
+// ADC Konversi ppm
 float baca_nilai_gas(int pin){
-  int nilaiADC = analogRead(pin);
-  int range = 1000;
-  float ppm = (range/32)*(3.3/5)*4096;
+  int range = analogRead(pin);
+  float ppm = (range/totalbit)*(vin/vref)*rangeADC;
   return ppm;
+}
+
+// read sensor api
+float baca_nilai_api(){
+  int fireStatus = digitalRead(readsensorfire);
+  return fireStatus;
 }
 
 // void loop
 void loop() {
-  int nilaiADCgas = baca_nilai_adc(readsensorgas);
+  float nilaiADCgas = baca_nilai_adc(readsensorgas);
+  float nilaippmgas = baca_nilai_gas(nilaiADCgas);
+  float nilaiapi = baca_nilai_api();
+  
+  // tampilkan pada serial print 
+  Serial.println(nilaippmgas);
+  Serial.println(nilaiADCgas);
+  // membuat kondisi dimana jika membaca adanya gas dan api 
 }
