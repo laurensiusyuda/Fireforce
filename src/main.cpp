9524c9d94dd66c1cf608b2d7cd4bc47cada8e9d0
Laurensius Yuda Wicaksono Nugroho <yudalaurensius87@gmail.com>
2023-09-04 17:41:02 +0700

    update time interupt

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+// TODO: Menambahkan fitur agar tidak ada delay dengan memberikan time interupt 
+
 #include <Arduino.h>
+// lib lcd I2C
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// inisialiasai pin LCD 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // inisialiasai sensor gas, sensor api, dan sensor ultrasonik
 #define readsensorgas 32
@@ -20,9 +28,18 @@ float ambang_nilai_gas_berbahaya = 5000.0;
 // menentukan ambang batas jarak api
 float ambang_jarak_api = 20.0;
 
+// membuat fungsi milis
+bool flagEksekusiLCD = false;
+int halaman = 1;
+unsigned long waktuEksekusiLCD = 0;
+int timecounter = 0;
+
 // void setup
 void setup() {
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
@@ -48,7 +65,7 @@ float baca_jarak() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   long duration = pulseIn(echoPin, HIGH);
-  // Calculate distance in centimeters based on the speed of sound
+  // mengubah jarak centimeter
   float distance = duration * 0.034 / 2;
   return distance;
 }
@@ -59,8 +76,30 @@ float baca_nilai_api(){
   return fireStatus;
 }
 
+// Fungsi untuk timer interrupt
+void timerInterrupt() {
+  if (millis() - waktuEksekusiLCD >= 1000) {
+    waktuEksekusiLCD = millis();
+    flagEksekusiLCD = true;
+    timecounter ++;
+
+    if (timecounter >= 5)
+    {
+      timecounter = 0;
+      halaman ++;
+      if (halaman == 4)
+      {
+        halaman = 1;
+      }
+    }
+  }
+  
+}
+
+
 // void loop
 void loop() {
+  // variabel menampilkan nilai adc dan data sensor 
   float nilaiADCgas = baca_nilai_adc(readsensorgas);
   float nilaippmgas = baca_nilai_gas(nilaiADCgas);
   float nilaiapi = baca_nilai_api();
@@ -70,6 +109,26 @@ void loop() {
   Serial.println(nilaippmgas);
   Serial.println(nilaiADCgas);
   Serial.println("Deteksi Api: " + String(nilaiapi == HIGH ? "Ya" : "Tidak"));
+
+  // tampilkan pada lcd sensor gas (halaman 1)
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("ADC Gas: ");      
+  lcd.print(nilaiADCgas);
+  lcd.setCursor(0, 1);
+  lcd.print("Nilai PPM Gas: ");      
+  lcd.print(nilaippmgas);
+  delay(1000);
+
+  // tampilkan pada lcd sensor api dan sensor ultrasonik (halaman 2)
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("ADC Gas: ");      
+  lcd.print(nilaiADCgas);
+  lcd.setCursor(0, 1);
+  lcd.print("Nilai PPM Gas: ");      
+  lcd.print(nilaippmgas);
+  delay(1000);
 
   // membuat pengkondisian
   // Kondisi untuk mendeteksi konsentrasi gas LPG
@@ -84,4 +143,70 @@ void loop() {
   }
 
   // membuat kondisi sensor api dengan sensor ultrasonik untuk mendetksi jarak api
+  if (jarak < ambang_jarak_api && nilaiapi == HIGH) {
+    Serial.println("Fire Detected at Close Range!");
+    // Add actions or alarms for fire detection at close range here.
+  } else if (nilaiapi == HIGH) {
+    Serial.println("Fire Detected!");
+    // Add actions or alarms for fire detection here.
+  } else {
+    Serial.println("No Fire Detected");
+  }
+  delay(1000); // Delay for 1 second before checking again
 }
+
+
+
+
+/*
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+!Inisialisasi objek LiquidCrystal_I2C dengan alamat I2C yang sesuai
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Sesuaikan dengan alamat LCD I2C Anda
+
+unsigned long previousMillis = 0;
+const unsigned long interval = 5000; // Interval perubahan data pada LCD dalam milidetik (5 detik)
+int currentPage = 1; // Menggunakan halaman 1 awalnya
+
+void setup() {
+  !Inisialisasi LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Page 1: Data 1");
+
+  !Inisialisasi pin ADC (misalnya, A0)
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    !Setiap 5 detik, ganti data pada halaman tertentu
+    if (currentPage == 1) {
+      lcd.setCursor(0, 1);
+      lcd.print("       "); // Hapus data sebelumnya
+      lcd.setCursor(0, 1);
+      lcd.print("Data 2");
+
+    !Pindah ke halaman berikutnya setelah mengganti data
+      currentPage = 2;
+    } else if (currentPage == 2) {
+      lcd.setCursor(0, 1);
+      lcd.print("       "); // Hapus data sebelumnya
+      lcd.setCursor(0, 1);
+      lcd.print("Data 3");
+
+    !Kembali ke halaman pertama setelah mengganti data pada halaman terakhir
+      currentPage = 1;
+    }
+  }
+
+  !Kode di loop utama dapat berjalan tanpa delay yang mempengaruhi tampilan LCD
+}
+
+*/
